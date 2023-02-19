f8ec99d8812f531c83ecc3a0b680d7f375c44236
David Buezas <david.buezas@gmail.com>
2023-02-19 01:03:00 +0100

    comments

--- src/state/state.tsx
+++ src/state/state.tsx
@@ -48,7 +48,7 @@ const userConfigState_vars = atom<string[]>({
 
   // The hack is to instantly update the userConfigState_vars array
   // otherwise it is imposible to accumulate the list of used variables in the family.
-  // This is because subsequent synchronous calls to set an atom don't update their previous state.
+  // This is because subsequent synchronous calls to set the vars array atom don't get an up to date array
   dangerouslyAllowMutability: true
 })
 
@@ -73,9 +73,9 @@ export const userConfigState = selectorFamily<string | undefined, string>({
           value === undefined
             ? without(vars_old, variable)
             : uniq([...vars_old, variable])
-        vars_old.length = 0
+        vars_old.length = 0 // Here the hack. Next set will get also the new vars
         vars_old.push(...vars_new)
-        return vars_new
+        return vars_new // but return a different reference to trigger rerener
       })
     }
 })
@@ -99,7 +99,6 @@ export const userConfigStateBulk = selector<Record<string, string | undefined>>(
         ...Object.keys(value)
       ])
       for (const variable of variables) {
-        // problem is here: userConfigStte needs the bulk and it sets it wrong
         set(userConfigState(variable), value[variable])
       }
     }
