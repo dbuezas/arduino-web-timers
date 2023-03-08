646d14cd29ad374e8fe3da4ab763c9e10001c767
David Buezas <david.buezas@gmail.com>
2023-03-08 17:03:16 +0100

    Fix types

--- src/state/state.tsx
+++ src/state/state.tsx
@@ -32,48 +32,35 @@ export const RegisterHashLink = () => {
   )
 }
 
-const userConfigState_vars = atom<string[]>(
-  []
+const userConfigState_vars = atom<string[]>([])
 
-  // The hack is to instantly update the userConfigState_vars array
-  // otherwise it is imposible to accumulate the list of used variables in the family.
-  // This is because subsequent synchronous calls to set the vars array atom don't get an up to date array
+const userConfigState_store = atomFamily((variable: string) =>
+  atom<string | undefined>(defaultState[variable])
 )
 
-const userConfigState_store = atomFamily<string, string | undefined>(
-  (variable) => atom(defaultState[variable])
+export const userConfigState = atomFamily((variable: string) =>
+  atom(
+    (get) => get(userConfigState_store(variable)),
+    (_get, set, value: string | undefined) => {
+      set(userConfigState_store(variable), value)
+      set(userConfigState_vars, (vars_old) =>
+        value === undefined
+          ? without(vars_old, variable)
+          : uniq([...vars_old, variable])
+      )
+    }
+  )
 )
 
-export const userConfigState = atomFamily<string, string | undefined>(
-  (variable: string) =>
-    atom(
-      (get) => get(userConfigState_store(variable)),
-      (get, set, value) => {
-        set(userConfigState_store(variable), value)
-        set(userConfigState_vars, (vars_old) => {
-          let vars_new =
-            value === undefined
-              ? without(vars_old, variable)
-              : uniq([...vars_old, variable])
-          // vars_old.length = 0 // Here the hack. Next set will get also the new vars
-          // vars_old.push(...vars_new)
-          return vars_new // but return a different reference to trigger rerener
-        })
-      }
-    )
-)
-
-export const userConfigStateBulk = atom<Record<string, string | undefined>>(
-  (get) => {
-    const got = Object.fromEntries(
+export const userConfigStateBulk = atom(
+  (get) =>
+    Object.fromEntries(
       get(userConfigState_vars).map((variable) => [
         variable,
         get(userConfigState(variable))
       ])
-    ) as Record<string, string>
-    return got
-  },
-  (get, set, value) => {
+    ),
+  (get, set, value: Record<string, string | undefined>) => {
     const variables = uniq([
       ...get(userConfigState_vars),
       ...Object.keys(value)
