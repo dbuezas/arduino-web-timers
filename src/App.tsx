f4e1b5b71c5d516b8393ad4775235822a4a4abf7
David Buezas <david.buezas@gmail.com>
2023-03-29 23:58:13 +0200

    updated setup-node

--- src/App.tsx
+++ src/App.tsx
@@ -57,7 +57,7 @@ const App = () => {
                     >
                       ARDUINO WEB
                       <br />
-                      TIMERS_
+                      TIMERS
                     </span>
                   </span>
                 </Navbar.Header>
